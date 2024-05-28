5139dc194ad70cb5ee1d3dae9c8447338b7590c3
tashisrael <tashisrael04@gmail.com>
2024-05-28 11:47:14 +0300

    add conatctslistpage class

--- contacts_app/lib/main.dart
+++ contacts_app/lib/main.dart
@@ -10,11 +10,19 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Contacts',
+      home: ContactsListPage(),
+    );
+  }
+}
+
+class ContactsListPage extends StatelessWidget {
+  const ContactsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Contacts'),),
     );
   }
 }
