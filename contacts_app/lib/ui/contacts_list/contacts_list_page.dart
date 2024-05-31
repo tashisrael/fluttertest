cff42f6f24c778b7d77cd7f8d1180f12cef2e082
tashisrael <tashisrael04@gmail.com>
2024-05-31 10:33:06 +0300

    create list of contacts

--- contacts_app/lib/ui/contacts_list/contacts_list_page.dart
+++ contacts_app/lib/ui/contacts_list/contacts_list_page.dart
@@ -1,10 +1,15 @@
+import 'package:contacts_app/data/contact.dart';
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
 
 class ContactsListPage extends StatelessWidget {
-  const ContactsListPage({super.key});
-
-
+  final List<Contact> _contacts = List.generate(50, (index) {
+    return Contact(
+      name: faker.person.firstName(), 
+      email: faker.internet.freeEmail(), 
+      phoneNumber: faker.randomGenerator.integer(1000000).toString(),);
+  },);
+ // const ContactsListPage({super.key});
   @override
   Widget build(BuildContext context) {
     final faker = Faker();
