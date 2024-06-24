6b1a4d8beb60f7433cecce6f16d9b8baff3f4bd3
tashisrael <tashisrael04@gmail.com>
2024-06-24 13:15:05 +0300

    update contact tile

--- contacts_app/lib/ui/contacts_list/widget/contact_tile.dart
+++ contacts_app/lib/ui/contacts_list/widget/contact_tile.dart
@@ -1,28 +1,36 @@
-import 'package:contacts_app/data/contact.dart';
+import 'package:contacts_app/ui/model/contacts_model.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ContactTile extends StatelessWidget {
   const ContactTile({
     super.key,
-    required this.contact,
-    required this.onFavoritePressed,
+    required this.contactIndex,
+    //required this.onFavoritePressed,
   });
 
-  final Contact contact;
-  final VoidCallback onFavoritePressed;
+  final int contactIndex;
+  //final VoidCallback onFavoritePressed;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(contact.name),
-      subtitle: Text(contact.email),
-      trailing: IconButton(
-        icon: Icon(
-          contact.isFavorite ? Icons.star : Icons.star_border,
-          color: contact.isFavorite ? Colors.amber : Colors.grey,
+    return ScopedModelDescendant<ContactsModel>(
+      builder: (context, child, model) {
+        final displayedContact = model.contacts[contactIndex];
+        return ListTile(
+        title: Text(displayedContact.name),
+        subtitle: Text(displayedContact.email),
+        trailing: IconButton(
+          icon: Icon(
+            displayedContact.isFavorite ? Icons.star : Icons.star_border,
+            color: displayedContact.isFavorite ? Colors.amber : Colors.grey,
+          ),
+          onPressed: () {
+            model.changeFavoriteStatus(contactIndex);
+          },
         ),
-        onPressed: onFavoritePressed,
-      ),
+      );
+      } 
     );
   }
 }
