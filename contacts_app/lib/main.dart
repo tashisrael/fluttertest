157afdb67e22a70fe99d92ecab0e849b81cfe148
tashisrael <tashisrael04@gmail.com>
2024-06-24 12:33:55 +0300

    update main.dart

--- contacts_app/lib/main.dart
+++ contacts_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:contacts_app/ui/contacts_list/contacts_list_page.dart';
+import 'package:contacts_app/ui/model/contacts_model.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,15 +11,18 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'Contacts',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(
-          color: Colors.blue,
+    return  ScopedModel(
+      model: ContactsModel(),
+      child: MaterialApp(
+        title: 'Contacts',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+            color: Colors.blue,
+          ),
         ),
+        home: const ContactsListPage(),
       ),
-      home: const ContactsListPage(),
     );
   }
 }
