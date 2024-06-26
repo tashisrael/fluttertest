e521ebff8ca5ee051d52e21773de0b6c46777c0f
tashisrael <tashisrael04@gmail.com>
2024-06-26 11:27:22 +0300

    add text labels

--- contacts_app/lib/ui/contact/widget/contact_form.dart
+++ contacts_app/lib/ui/contact/widget/contact_form.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 
 class ContactForm extends StatefulWidget {
-  const ContactForm({super.key});
+  //const ContactForm({super.key});
 
   @override
   State<ContactForm> createState() => _ContactFormState();
 }
 
 class _ContactFormState extends State<ContactForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name = '';
+  String _email = '';
+  String _phoneNumber = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: [
+          TextFormField(
+            onSaved: (value) => _name = value ?? '',
+            decoration: const InputDecoration(
+              labelText: 'Name'
+            ),
+          ),
+          TextFormField(
+            onSaved: (value) => _email = value ?? '',
+            decoration: const InputDecoration(
+              labelText: 'Email'
+            ),
+          ),
+          TextFormField(
+            onSaved: (value) => _phoneNumber = value ?? '',
+            decoration: const InputDecoration(
+              labelText: 'Phone Number'
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _formKey.currentState?.save();
+              print('Name: $_name, Email: $_email, Phone Number: $_phoneNumber');
+            },
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
   }
 }
