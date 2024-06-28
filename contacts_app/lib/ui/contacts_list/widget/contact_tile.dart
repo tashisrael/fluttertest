cc035d2edaa6367f5896d5f72d9087362503efd9
tashisrael <tashisrael04@gmail.com>
2024-06-28 14:24:47 +0300

    add slidable widget

--- contacts_app/lib/ui/contacts_list/widget/contact_tile.dart
+++ contacts_app/lib/ui/contacts_list/widget/contact_tile.dart
@@ -1,6 +1,7 @@
 import 'package:contacts_app/ui/contact/contact_edit_page.dart';
 import 'package:contacts_app/ui/model/contacts_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class ContactTile extends StatelessWidget {
@@ -17,24 +18,27 @@ class ContactTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final model = ScopedModel.of<ContactsModel>(context);
         final displayedContact = model.contacts[contactIndex];
-        return ListTile(
-        title: Text(displayedContact.name),
-        subtitle: Text(displayedContact.email),
-        trailing: IconButton(
-          icon: Icon(
-            displayedContact.isFavorite ? Icons.star : Icons.star_border,
-            color: displayedContact.isFavorite ? Colors.amber : Colors.grey,
+        return Slidable
+        (
+          child: ListTile(
+          title: Text(displayedContact.name),
+          subtitle: Text(displayedContact.email),
+          trailing: IconButton(
+            icon: Icon(
+              displayedContact.isFavorite ? Icons.star : Icons.star_border,
+              color: displayedContact.isFavorite ? Colors.amber : Colors.grey,
+            ),
+            onPressed: () {
+              model.changeFavoriteStatus(contactIndex);
+            },
           ),
-          onPressed: () {
-            model.changeFavoriteStatus(contactIndex);
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => ContactEditPage(editedContact: displayedContact, 
+              editedContactIndex: contactIndex),
+              ));
           },
-        ),
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (_) => ContactEditPage(editedContact: displayedContact, 
-            editedContactIndex: contactIndex),
-            ));
-        },
-      );
+          ),
+        );
       }
 }
