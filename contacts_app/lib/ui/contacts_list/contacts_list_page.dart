1e7429a080e7b0d8702b5d1ff180aa3464910f17
tashisrael <tashisrael04@gmail.com>
2024-07-24 12:25:34 +0300

    update contact model

--- contacts_app/lib/ui/contacts_list/contacts_list_page.dart
+++ contacts_app/lib/ui/contacts_list/contacts_list_page.dart
@@ -1,11 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:contacts_app/ui/contact/contact_create_page.dart';
 import 'package:contacts_app/ui/contacts_list/widget/contact_tile.dart';
 import 'package:contacts_app/ui/model/contacts_model.dart';
-import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 class ContactsListPage extends StatefulWidget {
-
   const ContactsListPage({super.key});
 
   @override
@@ -13,31 +12,23 @@ class ContactsListPage extends StatefulWidget {
 }
 
 class _ContactsListPageState extends State<ContactsListPage> {
-
-//build runs when the state changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Contacts'),),
+      appBar: AppBar(title: const Text('Contacts')),
       body: ScopedModelDescendant<ContactsModel>(
         builder: (context, child, model) {
           return ListView.builder(
-          itemCount: model.contacts.length,
-          itemBuilder: (context, index) {
-            return ContactTile(
-              contactIndex: index,
-              );
-          },
-        );
-        }
+            itemCount: model.contacts.length,
+            itemBuilder: (context, index) {
+              return ContactTile(contactIndex: index);
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.person_add),
         onPressed: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => const ContactCreatePage()),
-          // );
           Navigator.of(context).push(
             MaterialPageRoute(builder: (context) => const ContactCreatePage()),
           );
@@ -45,6 +36,4 @@ class _ContactsListPageState extends State<ContactsListPage> {
       ),
     );
   }
-
-  
 }
